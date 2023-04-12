6ce4a1ecd04707ba60e8d87908a4b9a46f886770
NSALAA9 <alaansairat99@gmail.com>
2023-04-12 11:34:29 +0000

    lab06

--- app.js
+++ app.js
@@ -1,28 +1,55 @@
-// Ask the user to enter his/her name as input
-const name = prompt("Please enter your name");
 
-// Ask the user to enter his/her gender as input
-const gender = prompt("Please enter your gender (male or female)");
-
-// Ask the user to enter his/her age as input and alert the user if the age is less than or equal to zero
-let age = prompt("Please enter your age");
-if (age <= 0) {
-  alert("Invalid age. Please enter a valid age.");
-}
-
-// Ask the user to confirm if he wants to skip the welcoming message
-const skipMessage = confirm("Do you want to skip the welcoming message?");
-
-// Alert a welcoming message with the name of the user and the title Mr if the user is male and Ms if the user is female, and if the gender input is not correct alert the welcoming message without the title, according to the confirmation
-let welcomeMessage;
-if (gender === "male") {
-  welcomeMessage = "Welcome, Mr " + name + "!";
-} else if (gender === "female") {
-  welcomeMessage = "Welcome, Ms " + name + "!";
-} else {
-  welcomeMessage = "Welcome, " + name + "!";
-}
-
-if (!skipMessage) {
-  alert(welcomeMessage);
-}
+  
+  
+  function getWelcomeMessage(name, gender) {
+    let title = '';
+    if (gender === 'male') {
+      title = 'Mr';
+    } else if (gender === 'female') {
+      title = 'Ms';
+    }
+    return `Welcome, ${title} ${name}!`;
+  }
+  
+  function getUserInput(question) {
+    let answer = prompt(question);
+    if (answer === '') {
+      answer = 'invalid';
+    }
+    return answer;
+  }
+  
+  function getAllAnswers() {
+    const answers = [];
+    answers.push(getUserInput('What is your name?'));
+    answers.push(getUserInput('What is your gender? (male/female)'));
+    answers.push(getUserInput('What is your age?'));
+    answers.push(getUserInput('Are you enjoying this program? (Yes/No)'));
+    answers.push(getUserInput('Do you find the program helpful? (Yes/No)'));
+    answers.push(getUserInput('Would you recommend the program to others? (Yes/No)'));
+    return answers;
+  }
+  
+  function displayAnswers(answers) {
+    for (let i = 0; i < answers.length; i++) {
+      console.log(`Answer ${i + 1}: ${answers[i]}`);
+    }
+  }
+  
+  const allAnswers = getAllAnswers();
+  const name = allAnswers[0];
+  const gender = allAnswers[1];
+  const age = parseInt(allAnswers[2]);
+  const skipMessage = false;
+  
+  if (age <= 0) {
+    alert('Invalid age! Please enter a valid age.');
+  } else {
+    const welcomeMessage = getWelcomeMessage(name, gender);
+    if (!skipMessage) {
+      alert(welcomeMessage);
+    }
+  
+    displayAnswers(allAnswers);
+  }
+  
